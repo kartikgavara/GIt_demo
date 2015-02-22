fcef5ac0e15f36ee1b10a0360d4dffa9df994df6
kartikgavara <kartikgavara@gmail.com>
2015-02-22 15:39:00 -0700

    Finish plots

--- ggplot.R
+++ ggplot.R
@@ -17,13 +17,36 @@ myplot <- ggplot(data = iris, aes(x=Sepal.Length, y=Sepal.Width))
 summary(myplot)
 ggplot(data = iris, aes(x=Sepal.Length, y=Sepal.Width, color=Species)) +
   geom_point(aes(shape=Species),size=4)
+
+
 d2 <- diamonds[sample(1:dim(diamonds)[1],1000), ]
 myplot <- ggplot(data = d2, aes(x=carat, y=price, color=color)) +
   geom_point(aes(shape=color),size=4)
 myplot
 dim(diamonds)
 diamonds
-library(MASS)
-ggplot(birthwt,aes(factor(race),bwt))+ geom_boxplot()
 
+
+library(MASS)
+myplot <- ggplot(birthwt,aes(factor(race),bwt))+ geom_boxplot()
+summary(myplot)
 birthwt
+
+
+# FACETS
+
+ggplot(iris,aes(Sepal.Length,Sepal.Width,color=Species))+geom_point(size=4)+
+  facet_grid(Species~.)
+ggplot(iris,aes(Sepal.Length,Sepal.Width,color=Species))+geom_point(size=4)+
+  facet_wrap(~Species)
+
+
+#Scales
+
+library(RColorBrewer)
+display.brewer.all()
+
+df<-melt(iris,id.vars="Species")
+ggplot(df,aes(Species,value,fill=variable))+
+  geom_bar(stat="identity",position="dodge")+
+  scale_fill_brewer(palette="Set1")
