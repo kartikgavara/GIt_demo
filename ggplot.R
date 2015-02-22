69b9eafe9cc17fa55ffc120665e89ebe74da2ab0
kartikgavara <kartikgavara@gmail.com>
2015-02-22 14:45:27 -0700

    BGG plot Box plot

--- ggplot.R
+++ ggplot.R
@@ -2,3 +2,28 @@
 #kartik
 #Feb 22, 2015:
 #kartikgavara@gmail.com
+
+install.packages("ggplot2",dependencies = TRUE)
+install.packages("plyr")
+install.packages("ggthemes")
+install.packages("reshape2")
+
+library("ggplot2")
+library("reshape2")
+
+library("ggthemes")
+library("plyr")
+myplot <- ggplot(data = iris, aes(x=Sepal.Length, y=Sepal.Width))
+summary(myplot)
+ggplot(data = iris, aes(x=Sepal.Length, y=Sepal.Width, color=Species)) +
+  geom_point(aes(shape=Species),size=4)
+d2 <- diamonds[sample(1:dim(diamonds)[1],1000), ]
+myplot <- ggplot(data = d2, aes(x=carat, y=price, color=color)) +
+  geom_point(aes(shape=color),size=4)
+myplot
+dim(diamonds)
+diamonds
+library(MASS)
+ggplot(birthwt,aes(factor(race),bwt))+ geom_boxplot()
+
+birthwt
